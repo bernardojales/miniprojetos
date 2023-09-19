3a991a4ad45440715ebada41ab357b34d312c356
João Pedro Miranda Cariry <berrytern@gmail.com>
2023-09-19 10:26:55 -0300

    [api-rest] - SANIC   - Add blueprint resource;

--- api-rest/sanic/server.py
+++ api-rest/sanic/server.py
@@ -1,8 +1,15 @@
-from sanic import Sanic # import Sanic class that represents our API REST application
+from sanic import Sanic, Request, response # import Sanic class that represents our API REST application
 
 app = Sanic("http") # instanciate our API REST application
 
-app.route(uri="/petshop", method=["GET"])
+@app.route(uri="/petshop", method=["GET"]) # the basic way to register a endpoint handler
+async def get_petshop(request: Request):
+    response.text(body="Ok", status=200, headers={}) # Sends a request response with content-type header set as 'text/plain; charset=utf-8'
+    # response.json({"message": "Ok"}, status=200, headers={}) # Sends a request response with content-type header set as 'application/json'
+
+# most recommended way to register a endpoint handler
+from src.routes import ROUTES # import blueprint
+app.blueprint(ROUTES, version=1)
 
 if __name__ == "__main__":
     app.run(# run the application passing information
---
+++ api-rest/sanic/src/routes/petshop_route.py
@@ -0,0 +1,24 @@
+from sanic import Blueprint, Request, response # import the blueprint class
+
+PETSHOP_ROUTE = Blueprint("petshop") # instantiate a blueprint setting his name
+
+#                       uri,     methods    
+@PETSHOP_ROUTE.route("/petshop", methods=["GET"]) # define handler to endpoint when METHOD == GET
+async def get_petshop2(request: Request):
+    response.json([{"name": "Villa Petshop"}]) # the default status code is 200 and headers are set as {}, we can ommity this parameters
+
+
+@PETSHOP_ROUTE.route("/petshop", methods=["POST"]) # define handler to endpoint when METHOD == POST
+async def get_petshop2(request: Request):
+    # LETS GET THE REQUEST BODY CONTENT
+    # request.body # get body of request in format bytes
+    # request.json # get body of request in json format
+    body = request.json or {}
+    print(body)
+    # LETS ADD SOME VALIDATION FOR THE INPUT CONTENT
+    # just accept requests that contain name field
+    if "name" in body and not (set(body) - set({"name"})):
+        response.json(body) # the default status code is 200 and headers are set as {}, we can ommity this parameters
+    else:
+        # Sends a error response indicating that the request body is invalid
+        response.json({"message": "invalid body", "description": "must only pass the required fields: name"}, status=400)
