bf5137aabadd83360081e7a4a37fafae766aa0bf
João Pedro Miranda Cariry <berrytern@gmail.com>
2023-09-19 11:21:32 -0300

    [api-rest] - SANIC   - Add environment variables;

--- api-rest/sanic/server.py
+++ api-rest/sanic/server.py
@@ -8,11 +8,15 @@ async def get_petshop(request: Request):
     # response.json({"message": "Ok"}, status=200, headers={}) # Sends a request response with content-type header set as 'application/json'
 
 # most recommended way to register a endpoint handler
-from src.routes import ROUTES # import blueprint
+from src.routes import ROUTES # import blueprint group
 app.blueprint(ROUTES, version=1)
 
 if __name__ == "__main__":
+    from src.utils import settings
     app.run(# run the application passing information
-        host="0.0.0.0", # from what network interfaces it must be able to answer
-        port=3000, # from what port it must be able to listen
+        # host="0.0.0.0", # from what network interfaces it must be able to answer
+        # port=3000, # from what port it must be able to listen
+        # use environment variables
+        host=settings.HOST,
+        port= settings.PORT_HTTP
     ) 
